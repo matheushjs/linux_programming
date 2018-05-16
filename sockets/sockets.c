e0a550cbe84127289c92d4a8d0e64bef2193f2c1
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-16 19:33:34 -0300

    Successfully get addrinfo on google

--- sockets/sockets.c
+++ sockets/sockets.c
@@ -2,8 +2,9 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]){
+void do_some_filling(){
 	int binaddr;
 	inet_pton(AF_INET, "172.217.29.132", &binaddr); // presentation to network (text to binary)
 	                   // The above is google's IP
@@ -28,7 +29,26 @@ int main(int argc, char *argv[]){
 
 		.ai_next = 0
 	};
+}
 
+void get_some_addrinfo(){
+	struct addrinfo hints = {
+		.ai_family = AF_UNSPEC, // tanto faz IPv4 ou v6
+		.ai_socktype = SOCK_STREAM, // socket com conexão
+		.ai_protocol = 0, // qualquer protocolo
+		.ai_flags = AI_PASSIVE // Preenche meu IP automaticamente
+	};
+	
+	struct addrinfo *res;
 
+	int retval = getaddrinfo("www.google.com", "http", &hints, &res);
+
+	printf("Retval: %d\n", retval);
+
+	freeaddrinfo(res);
+}
+
+int main(int argc, char *argv[]){
+	get_some_addrinfo();
 	return 0;
 }
